0a079268b8bd23be80a25dbb15c7df3001ce899a
sarahmr63089 <sarahrose63089@gmail.com>
2020-07-20 15:14:18 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,98 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = Category.create([
+  {
+    name: "cute"
+  },
+  {
+    name: "funny"
+  },
+  {
+    name: "sad"
+  },
+  {
+    name: "heart-warming"
+  }
+])
+
+posts = Post.create([
+  {
+    title: "This is a Dog",
+    content: "dogs are so cute"
+  },
+  {
+    title: "What to do?",
+    content: "life can be sad"
+  },
+  {
+    title: "US",
+    content: "US sees some crazy stuff"
+  },
+  {
+    title: "Apples",
+    content: "apples belong in apple pie"
+  }
+])
+
+users = User.create([
+  {
+    username: "peanuts",
+    email: "peanuts@yahoo.com"
+  },
+  {
+    username: "lazy",
+    email: "lazy@gmail.com"
+  },
+  {
+    username: "horses",
+    email: "horses@yahoo.com"
+  },
+  {
+    username: "lifeisgood",
+    email: "lifeisgood@gmail.com"
+  }
+])
+
+comments = Comment.create([
+  {
+    content: "this sucks",
+    user_id: 1,
+    post_id: 1
+  },
+  {
+    content: "so so",
+    user_id: 1,
+    post_id: 1
+  },
+  {
+    content: "ok, cool",
+    user_id: 2,
+    post_id: 2
+  },
+  {
+    content: "let's go!",
+    user_id: 2,
+    post_id: 3
+  }
+])
+
+post_categories = PostCategory.create([
+  {
+    post_id: 1,
+    category_id: 1
+  },
+  {
+    post_id: 1,
+    category_id: 2
+  },
+  {
+    post_id: 2,
+    category_id: 1
+  },
+  {
+    post_id: 3,
+    category_id: 1
+  }
+])
